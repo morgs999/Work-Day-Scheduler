abfa805a97286be6ed004a48bf7b6c835e355f93
Morgan <morgs99@gmail.com>
2023-11-24 21:34:39 -0500

    ask bcs question

--- Develop/script.js
+++ Develop/script.js
@@ -1,23 +1,57 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+  // CURRENT DATE & TIME DISPLAY //
+  let timeDisplayEl = $('#currentDay');
+  function displayTime() {
+    let rightNow = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
+    timeDisplayEl.text(rightNow);
+  }
+
+  // BLUE SAVE BUTTON FUNCTIONALITY //
+  let saveBtn = $('.saveBtn');
+  saveBtn.on('click', function () {
+    let hour = $(this).parent().attr('id');
+    let value = $(this).siblings('.description').val();
+    localStorage.setItem(hour, value);
+  });
+
+  // $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+  // $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+  // $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+  // $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+  // $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+  // $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+  // $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+  // $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+  // $("#hour-17 .description").val(localStorage.getItem("hour-17"));
+
+  function textBlocks() {
+    $(".description").each(function () {
+      let timeBlock = (this.id);
+      $(this).val(localStorage.getItem("value"));
+      
+    })
+  }
+
+  // PAST, PRESENT, FUTURE COLOR CODING //
+  function colorIn() {
+    // can use dayjs('2020-01-01 12:00:00') to check colors
+    let currentHour = dayjs('2020-01-01 12:00:00').hour();
+    $(".time-block").each(function () {
+      let timeBlock = (this.id);
+
+      if (currentHour > timeBlock) {
+        $(this).addClass('past');
+      } else if (currentHour == timeBlock) {
+        $(this).addClass('present')
+      } else {
+        $(this).addClass('future');
+      }
+    })
+  }
+
+  // FUNCTIONS RUNNING //
+  displayTime();
+  setInterval(displayTime, 1000);
+  textBlocks();
+  colorIn();
 });
---
+++ README.md
@@ -0,0 +1,4 @@
+# Work Day Scheduler
+
+## Description
+
